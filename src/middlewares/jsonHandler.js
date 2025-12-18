4311007ff45bd15e719ece0cfb7963d474710430
Eric Ramalho <ericramalho@hotmail.com>
2025-12-18 16:31:08 -0300

    git init

---
+++ src/middlewares/jsonHandler.js
@@ -0,0 +1,15 @@
+export async function jsonHalndler(request, response) {
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+  try {
+    // Adicionando dentro da requisição a propriedad body para montar os chunks
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (error) {
+    request.body = null;
+  }
+
+  response.setHeader("Content-Type", "application/json");
+}
