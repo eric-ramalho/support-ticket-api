50632173b910362e483838579c5548f9cd024aeb
Eric Ramalho <ericramalho@hotmail.com>
2025-12-19 09:14:23 -0300

    middleware de rotas

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import http from "http";
 
 import { jsonHalndler } from "./middlewares/jsonHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
 
 async function listener(request, response) {
   await jsonHalndler(request, response);
-  console.log(request.body);
+  routeHandler(request, response);
 }
 
 http.createServer(listener).listen(3333);
