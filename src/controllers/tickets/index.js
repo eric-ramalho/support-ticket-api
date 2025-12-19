fb58d23b49a6ac2ace7fce87c45adc2504c892e8
Eric Ramalho <ericramalho@hotmail.com>
2025-12-19 12:14:36 -0300

    Exibindo todos os tickets

---
+++ src/controllers/tickets/index.js
@@ -0,0 +1,14 @@
+/*
+Nomes para arquivo de controler
+
+CREATE - CRIAR
+INDEX - LISTAR
+UPDATE - ATUALIZAR
+REMOVE - REMOVER
+SHOW - EXIBIR UM UNICO REGISTRO
+*/
+
+export function index({ request, response, database }) {
+  const tickets = database.select("tickets");
+  return response.end(JSON.stringify(tickets));
+}
--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,10 +1,15 @@
 import { create } from "../controllers/tickets/ceate.js";
-
+import { index } from "../controllers/tickets/index.js";
 // separando as rotas de ticket em um unico arquivo
 export const tickets = [
   {
     method: "POST",
     path: "/tickets",
     controller: create,
+  },
+  {
+    method: "GET",
+    path: "/tickets",
+    controller: index,
   },
 ];
