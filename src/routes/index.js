034a22d38f44583da546905cc3f9ee5fdb692bd0
Eric Ramalho <ericramalho@hotmail.com>
2025-12-22 15:27:24 -0300

    Parametros dinamicos com regex

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,9 @@
 // Responsavel por juntar todas as rotas
+import path from "node:path";
 import { tickets } from "./tickets.js";
+import { parseRoutePath } from "../utils/parseRoutePath.js";
 
-export const routes = [...tickets];
+export const routes = [...tickets].map((route) => ({
+  ...route,
+  path: parseRoutePath(route.path),
+}));
