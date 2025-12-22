4862c0440c03400c52c3d2f705f807674bbe3fd1
Eric Ramalho <ericramalho@hotmail.com>
2025-12-22 15:36:13 -0300

    Obtendo parâmetros das rotas

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -9,6 +9,8 @@ SHOW - EXIBIR UM UNICO REGISTRO
 */
 
 export function index({ request, response, database }) {
+  const { status } = request.query;
+  console.log(status);
   const tickets = database.select("tickets");
   return response.end(JSON.stringify(tickets));
 }
